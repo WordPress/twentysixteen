798c543fe173ef367b44e9a9d478978052e65b3d
David A. Kennedy <me@davidakennedy.com>
2015-09-21 16:46:44 -0400

    Add `aria-label`s to `nav` elements for better specificity
    * This helps these landmarks with the same role be more specific and useful to users with assistive technology.
    * I've used the word "Footer" to make the ones in the `footer` be unique, but we may prefer a different word choice.
    
    Resolves #236

--- header.php
+++ header.php
@@ -43,7 +43,7 @@
 
 					<div id="site-header-menu" class="site-header-menu">
 						<?php if ( has_nav_menu( 'primary' ) ) : ?>
-							<nav id="site-navigation" class="main-navigation" role="navigation">
+							<nav id="site-navigation" class="main-navigation" role="navigation" aria-label='<?php _e( 'Primary Menu', 'twentysixteen' ); ?>'>
 								<?php
 									wp_nav_menu( array(
 										'theme_location' => 'primary',
@@ -54,7 +54,7 @@
 						<?php endif; ?>
 
 						<?php if ( has_nav_menu( 'social' ) ) : ?>
-							<nav id="social-navigation" class="social-navigation" role="navigation">
+							<nav id="social-navigation" class="social-navigation" role="navigation" aria-label='<?php _e( 'Social Links Menu', 'twentysixteen' ); ?>'>
 								<?php
 									wp_nav_menu( array(
 										'theme_location' => 'social',
