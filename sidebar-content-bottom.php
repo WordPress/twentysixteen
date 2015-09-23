880921432d01900e45a9a94990e3f756ae7272f6
Ehtisham Siddiqui <ehtishams@gmail.com>
2015-09-23 13:10:28 +0500

    comment typo sidebar-content-bottom.php

--- sidebar-content-bottom.php
+++ sidebar-content-bottom.php
@@ -13,7 +13,7 @@ if ( ! is_active_sidebar( 'sidebar-2' ) && ! is_active_sidebar( 'sidebar-3' ) ) 
 	return;
 }
 
-// If we get this far, we have widgets. Let do this.
+// If we get this far, we have widgets. Let's do this.
 ?>
 <div id="content-bottom-widgets" class="content-bottom-widgets" role="complementary">
 	<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
