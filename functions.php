658f1b2ec01ef97c169ad4f9043f8c2eeeb39bb2
Chandra Maharzan <chandra@graphpaperpress.com>
2015-09-24 17:30:27 +0545

    update version number

--- functions.php
+++ functions.php
@@ -221,7 +221,7 @@ function twentysixteen_scripts() {
 	wp_enqueue_style( 'twentysixteen-fonts', twentysixteen_fonts_url(), array(), null );
 
 	// Add Genericons, used in the main stylesheet.
-	wp_enqueue_style( 'genericons', get_template_directory_uri() . '/genericons/genericons.css', array(), '3.3' );
+	wp_enqueue_style( 'genericons', get_template_directory_uri() . '/genericons/genericons.css', array(), '3.4' );
 
 	wp_enqueue_style( 'twentysixteen-style', get_stylesheet_uri() );
 
