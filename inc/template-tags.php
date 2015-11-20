1c5198a1ce00df873bbb539b9600b0f9d029fa6a
Takashi Irie <takashi@automattic.com>
2015-11-20 12:49:28 +0000

    Edit Translation String for Continue Reading

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -181,7 +181,7 @@ function twentysixteen_excerpt_more() {
 	$link = sprintf( '<a href="%1$s" class="more-link">%2$s</a>',
 		esc_url( get_permalink( get_the_ID() ) ),
 		/* translators: %s: Name of current post */
-		sprintf( __( 'Continue reading %s', 'twentysixteen' ), '<span class="screen-reader-text">' . get_the_title( get_the_ID() ) . '</span>' )
+		sprintf( __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ), get_the_title( get_the_ID() ) )
 	);
 	return ' &hellip; ' . $link;
 }
--- template-parts/content.php
+++ template-parts/content.php
@@ -25,7 +25,7 @@
 		<?php
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
-				__( 'Continue reading<span class="screen-reader-text"> %s</span>', 'twentysixteen' ),
+				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
 				get_the_title()
 			) );
 
