506aa16451ec75dd29039672204f0d49bfebc266
David A. Kennedy <me@davidakennedy.com>
2016-07-10 14:29:35 -0400

    Update translation project URL to be more specific.

--- functions.php
+++ functions.php
@@ -47,7 +47,7 @@ if ( ! function_exists( 'twentysixteen_setup' ) ) :
 function twentysixteen_setup() {
 	/*
 	 * Make theme available for translation.
-	 * Translations can be filed at WordPress.org. See: https://translate.wordpress.org/
+	 * Translations can be filed at WordPress.org. See: https://translate.wordpress.org/projects/wp-themes/twentysixteen
 	 * If you're building a theme based on Twenty Sixteen, use a find and replace
 	 * to change 'twentysixteen' to the name of your theme in all the template files
 	 */
